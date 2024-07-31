f50954fb294f73345037f2a09e8874d6603edea4
shivani091995 <shivani08das@gmail.com>
2024-07-31 23:04:43 +0530

    add middleware to handle errors

--- api/index.js
+++ api/index.js
@@ -19,6 +19,19 @@ mongoose.connect(process.env.MONGO, {
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 
+
+//MiddleWare
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error'
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    })
+})
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
